09e57f45b34d45f3fce043e819362619cda67027
4010c2 <4010c2@mango.ece.uprm.edu>
2017-02-08 13:27:18 -0400

    Hice lo que dijo hasta la parte 7

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,15 @@
 import javax.swing.JFrame;
 
+import mypanelclass.MyPanelClass;
+
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("David Carrion Beniquez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 430);
 		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
