c0738a537ab00870c54fbac9e1be5175bd044069
4010c2 <4010c2@mango.ece.uprm.edu>
2017-02-08 12:09:19 -0400

    What's new?
    The window's size is now 200*200 and appears somewhat in the center.
    The title bar of the window is my name!
    The program terminates when the window is closed.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("David Carrion Beniquez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(900, 430);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
