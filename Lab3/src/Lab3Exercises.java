a48c28b7e6bff4174816cfdb09304ab9a3543db6
4010c2 <4010c2@mango.ece.uprm.edu>
2017-02-08 12:17:48 -0400

    what's new?
    The window is 200*200 and opens somewhat in the middle.
    The title bar says my name!
    The program terminates when the window is closed.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("David Carrion Beniquez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 431);
+		myFrame.setLocation(900, 430);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
