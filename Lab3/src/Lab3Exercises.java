6b0fdf6619158e0707d391d0a8aa061de8e21708
4010c2 <4010c2@aguacate.ece.uprm.edu>
2017-11-15 16:21:44 -0400

    The title of the frame is my name, the size is 200x200, and it opens in the center of the screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,17 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("David Carrion Beniquez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Dimension screenSize= Toolkit.getDefaultToolkit().getScreenSize();
+		int width=(int)screenSize.getWidth()/2;
+		int height= (int)screenSize.getHeight()/2;
+		myFrame.setLocation(width-100,height-100 );
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
